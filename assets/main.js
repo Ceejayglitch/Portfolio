848f07e552bcf99a54cd8ecf3e943b127fcd8022
Criston <cristonjade@gmail.com>
2024-02-22 19:42:59 +0800

    smol edits

--- assets/main.js
+++ assets/main.js
@@ -57,8 +57,32 @@ function handleScroll() {
     });
 }
 
-// Add event listener for scroll events
-window.addEventListener('scroll', handleScroll);
 
-// Initial check in case elements are already in view
-handleScroll();
+// basta smooth scrolling to
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
+
+//   function para sa hamburger scroll 
+
